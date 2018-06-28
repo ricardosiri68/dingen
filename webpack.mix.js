4eeb539c315c1e54346f7dec171ee4e2feb54fac
Richar Siri <rsi@3818.com.ar>
2018-06-28 14:38:08 -0300

    alto commit

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,13 +2,14 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin')
 let mix = require('laravel-mix');
 
 mix
-	.setPublicPath('dingen_pyramid/static')
-	.setResourceRoot('/static/')
-	.ts('dingen_pyramid/resources/ts/app.ts', 'js')
-	.sass('dingen_pyramid/resources/sass/app.scss', 'css')
+  .setPublicPath('dingen_pyramid/static')
+  .setResourceRoot('/static/')
+  .copyDirectory('dingen_pyramid/resources/images', 'dingen_pyramid/static/images')
+  .js('dingen_pyramid/resources/js/app.js', 'js')
+  .sass('dingen_pyramid/resources/sass/app.scss', 'css')
 	.webpackConfig({
 		plugins: [
 			new VueLoaderPlugin()
-		],
+		]
 	})
-	.version()
+  .version()
